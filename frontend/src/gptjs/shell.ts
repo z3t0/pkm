d6fc724150a7a5777fb4263e84e06ab8f3276d56
Rafi Khan <rafi@rafikhan.io>
2024-12-12 00:08:47 -0500

    ask with context

--- frontend/src/gptjs/shell.ts
+++ frontend/src/gptjs/shell.ts
@@ -9,9 +9,23 @@ async function ask(question: string) {
   const answer =
     await ollama_client.generateCompletion(question)
 
-  console.log("Question: " + question,
+  console.log("Question: " + question, '\n',
     "Answer: " + answer)
 }
 
+async function askWithContext(context: string, question: string) {
+
+  const prompt = `Given the following context: "` +
+    context + `.
+
+==================
+
+Answer the following question: "`+
+question
+
+  const answer = await ask(prompt);
+}
+
 ask("How many provinces are in canada?")
+askWithContext("It is december", "How many days are in the current month?")
 
